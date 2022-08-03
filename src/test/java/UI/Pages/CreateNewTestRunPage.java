76319500c1dcd3a753c2ec23b10b7f9200bc31cb
Denis Kachan <den.kachan.98@mail.ru>
2022-08-04 00:05:39 +0300

    Adding JavaScriptExecutor to the testRunCreation Step

--- src/test/java/UI/Pages/CreateNewTestRunPage.java
+++ src/test/java/UI/Pages/CreateNewTestRunPage.java
@@ -1,12 +1,15 @@
 package UI.Pages;
 
 import Utils.TestConstants;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.webdriver;
 
 @Log4j2
 public class CreateNewTestRunPage implements TestConstants {
@@ -23,6 +26,7 @@ public class CreateNewTestRunPage implements TestConstants {
         log.info("Set {} as a title",testRunTitle);
         title.sendKeys(testRunTitle);
         log.info("Click add cases button");
+        Selenide.executeJavaScript("window.scrollBy(0,600)");
         addCasesButton.click();
         log.info("Select test case");
         selectTestCaseCheckBox.click();
